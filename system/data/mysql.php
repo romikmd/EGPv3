c450d45259bc3625e0020b9072d9d989d70552cd
MaXa0H <maxaoh.forever.1999@gmail.com>
2022-02-09 20:19:20 +0300

    Update mysql.php

--- system/data/mysql.php
+++ system/data/mysql.php
@@ -3,7 +3,7 @@
 		exit(header('Refresh: 0; URL=http://'.$_SERVER['SERVER_NAME'].'/404'));
 
 	DEFINE('USER_DATABASE', 'root');
-	DEFINE('PASSWORD_DATABASE', 'MYSQLPASS'); // пароль mysql
+	DEFINE('PASSWORD_DATABASE', 'SQLPASS'); // пароль mysql
 	DEFINE('NAME_DATABASE', 'enginegp');
 	DEFINE('CONNECT_DATABASE', 'localhost');
 	DEFINE('ERROR_DATABASE', FALSE);
